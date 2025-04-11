50a81737a0a06c7ecc1671c205adf3d4643ddba9
Oleksandr Bolokhovetskyi <o.bolokhovetskyi@mayabproject.com>
2025-04-11 12:04:09 +0300

    Fixed week count (still have some problems here)

--- count_messages_by_timeperiod.py
+++ count_messages_by_timeperiod.py
@@ -2,7 +2,7 @@ import json
 import sys
 import csv
 from collections import defaultdict
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def count_messages_by_time(time_unit, json_filename, filter_from_id=None):
     with open(json_filename, "r", encoding="utf-8") as f:
@@ -25,7 +25,8 @@ def count_messages_by_time(time_unit, json_filename, filter_from_id=None):
         elif time_unit == "month":
             time_key = dt.strftime("%Y/%m")
         elif time_unit == "week":
-            time_key = f"{dt.strftime('%Y')}/W{dt.strftime('%U')}"
+            week_start = dt - timedelta(days=dt.weekday())  # Start of the week (Monday)
+            time_key = f"{week_start.strftime('%Y/%m')}/W{week_start.strftime('%U')}"
         else:
             print("Invalid time unit. Use: year, month, or week.", file=sys.stderr)
             sys.exit(1)
@@ -40,7 +41,7 @@ def count_messages_by_time(time_unit, json_filename, filter_from_id=None):
 
 if __name__ == "__main__":
     if len(sys.argv) < 3 or len(sys.argv) > 4:
-        print("Usage: python script.py <time_unit> <json_filename> [from_id]", file=sys.stderr)
+        print("Usage: python3 count_messages_by_timeperiod.py (year|month|week) <json_filename> [from_id]", file=sys.stderr)
         sys.exit(1)
     
     time_unit = sys.argv[1].lower()
@@ -48,3 +49,4 @@ if __name__ == "__main__":
     filter_from_id = sys.argv[3] if len(sys.argv) > 3 else None
     
     count_messages_by_time(time_unit, json_filename, filter_from_id)
+
